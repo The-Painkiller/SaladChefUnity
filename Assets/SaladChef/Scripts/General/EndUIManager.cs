39354c2161b240364391031aaf1eadabfa116cc8
Shashank Sharma <artofshashank@gmail.com>
2019-09-01 12:52:50 +0530

    - EndGame

---
+++ Assets/SaladChef/Scripts/General/EndUIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// MonoBehaviour Class for implementing End Screen of the game.
+/// Interacts with GameManager to display winner.
+/// Also with help of HighScoreManager, displays past high scores.
+/// </summary>
+public class EndUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _winnerText;
+    [SerializeField]
+    private Text [] _highScores;
+    [SerializeField]
+    private Text [] _highScoreNames;
+
+    public GameObject _endGameScreen;
+    
+    /// <summary>
+    /// Called by GameManager. Activates _endGameScreen GameObject that has end screen UI.
+    /// </summary>
+    /// <param name="highscore">Takes current player high score</param>
+    /// <param name="PlayerName">Takes current high scoring player</param>
+    public void DisplayEndGame ( int highscore, string PlayerName )
+    {
+        _winnerText.text = "Winner: " + PlayerName + "(" + highscore.ToString ( ) + ")";
+        _endGameScreen.SetActive ( true );
+        HighScoreManager.SetScores ( highscore, PlayerName );
+        DisplayHighScores ( );
+    }
+
+    /// <summary>
+    /// Simple level load call that just restarts the scene.
+    /// Called from a button in end screen UI.
+    /// </summary>
+    public void Restart ( )
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene ( 0 );
+    }
+
+    /// <summary>
+    /// Displays the list of past high scores.
+    /// </summary>
+    private void DisplayHighScores ( )
+    {
+        for ( int i = 0; i < _highScores.Length; i++ )
+        {
+            _highScores [ i ].text = HighScoreManager.GetScores ( ) [ i ].ToString();
+            _highScoreNames [ i ].text = HighScoreManager.GetNames ( ) [ i ] + " : ";
+        }
+    }
+}
---
+++ Assets/SaladChef/Scripts/General/HighScoreManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Class with static functions to get or set current high scores and past high score lists.
+/// </summary>
+public class HighScoreManager
+{
+
+    /// <summary>
+    /// Max number of past high scores to be saved.
+    /// </summary>
+    private static int maxScoresSaved = 10;
+
+    /// <summary>
+    /// Scores array, that uses PlayerPrefsX class to store int [] to PlayerPrefs.
+    /// </summary>
+    private static int [] Scores
+    {
+        get
+        {
+            return PlayerPrefsX.GetIntArray ( "Scores", 0, maxScoresSaved );
+        }
+        set
+        {
+            PlayerPrefsX.SetIntArray ( "Scores", value );
+        }
+    }
+
+    /// <summary>
+    /// Player names array, that uses PlayerPrefsX class to store string [] to PlayerPrefs.
+    /// </summary>
+    private static string [] PlayerNames
+    {
+        get
+        {
+            return PlayerPrefsX.GetStringArray ( "PlayerNames", "--", maxScoresSaved );
+        }
+        set
+        {
+            PlayerPrefsX.SetStringArray ( "PlayerNames", value );
+        }
+    }
+
+    /// <summary>
+    /// Returns past Scores array.
+    /// </summary>
+    /// <returns></returns>
+    public static int [] GetScores ( )
+    {
+        return Scores;
+    }
+
+    /// <summary>
+    /// Returns past Scorer names array.
+    /// </summary>
+    /// <returns></returns>
+    public static string [] GetNames ( )
+    {
+        return PlayerNames;
+    }
+
+    /// <summary>
+    /// Checks and sets current high score and player into the past high scores list.
+    /// </summary>
+    /// <param name="currentHighScore">Takes current game's high score.</param>
+    /// <param name="currentScorerName">Takes current game's high scorer's list.</param>
+    public static void SetScores ( int currentHighScore, string currentScorerName)
+    {
+        int leastHighScore = Scores.Min ( );
+        if ( leastHighScore > currentHighScore )
+            return;
+
+        int [] scoresTemp = Scores;
+        string [] namestemp = PlayerNames;
+
+        for ( int i = 0; i < scoresTemp.Length; i++ )
+        {
+            if ( leastHighScore == scoresTemp [ i ] )
+            {
+                scoresTemp [ i ] = currentHighScore;
+                namestemp [ i ] = currentScorerName;
+                break;
+            }
+        }
+
+        Scores = scoresTemp;
+        PlayerNames = namestemp;
+    }
+}
