704f25672ad906d0b19702ebeebac01d18b779aa
Shashank Sharma <artofshashank@gmail.com>
2019-09-01 12:45:48 +0530

    - Input Implementation

---
+++ Assets/SaladChef/Scripts/General/MoveableAreaConstants.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Static class with basic moveable area coordinates.
+/// </summary>
+public static class MoveableAreaConstants
+{
+	public static readonly Vector2 _playerMoveableAreaMax = new Vector2 (5.5f, 1.5f);
+	public static readonly Vector2 _playerMoveableAreaMin = new Vector2 (-5.5f, -2.5f);
+    public static readonly Vector3 _firstPlayerSpawnPoint = new Vector3 ( -2.5f, -2.5f, -0.01f );
+    public static readonly Vector3 _secondPlayerSpawnPoint = new Vector3 ( 2.5f, -2.5f, -0.01f );
+
+    /// <summary>
+    /// Mainly used by PickupsManager to get a random location in playable area to keep a pickup at.
+    /// </summary>
+    /// <returns></returns>
+    public static Vector2 GetRandomPlacement ( )
+    {
+        Vector2 placement;
+
+        float x = (int)Random.Range ( -4, 5 );
+        float y = (int)Random.Range ( -1, 1 );
+
+        x -= 0.5f;
+        y -= 0.5f;
+        placement = new Vector2 ( x, y );
+
+        return placement;
+    }
+}
---
+++ Assets/SaladChef/Scripts/Input/InputControl.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// InputControl class extending from IInputControl Interface.
+/// Used to map keyboard keys passed from GameManager, to different actions.
+/// </summary>
+public class InputControl : MonoBehaviour, IInputControl
+{
+    public Action ForwardPressed { get; set; }
+    public Action BackwardPressed { get; set; }
+    public Action LeftPressed { get; set; }
+    public Action RightPressed { get; set; }
+    public Action InteractionPressed { get; set; }
+
+	private KeyCode _forwardKey;
+	private KeyCode _backwardKey;
+	private KeyCode _leftKey;
+	private KeyCode _rightKey;
+	private KeyCode _interactionKey;
+
+    /// <summary>
+    /// Called to assign input keyboard keys for different actions.
+    /// </summary>
+    /// <param name="fwd">Key for forward movement.</param>
+    /// <param name="bck">Key for backward movement.</param>
+    /// <param name="rgt">Key for right movement.</param>
+    /// <param name="lft">Key for left movement</param>
+    /// <param name="intrct">Key for interaction.</param>
+	public void AssignInputs (KeyCode fwd, KeyCode bck, KeyCode rgt, KeyCode lft, KeyCode intrct)
+	{
+		_forwardKey = fwd;
+		_backwardKey = bck;
+		_leftKey = lft;
+		_rightKey = rgt;
+		_interactionKey = intrct;
+	}
+
+	void Update()
+    {
+        if ( Input.GetKeyDown ( _forwardKey ) )
+        {
+            if ( ForwardPressed != null )
+            {
+                ForwardPressed.Invoke ( );
+            }
+        }
+
+        if ( Input.GetKeyDown ( _backwardKey ) )
+        {
+            if ( BackwardPressed != null )
+            {
+                BackwardPressed.Invoke ( );
+            }
+        }
+
+        if ( Input.GetKeyDown ( _leftKey ) )
+        {
+            if ( LeftPressed != null )
+            {
+                LeftPressed.Invoke ( );
+            }
+        }
+
+        if ( Input.GetKeyDown ( _rightKey ) )
+        {
+            if ( RightPressed != null )
+            {
+                RightPressed.Invoke ( );
+            }
+        }
+
+        if ( Input.GetKeyDown ( _interactionKey ) )
+        {
+            if ( InteractionPressed != null )
+            {
+                InteractionPressed.Invoke ( );
+            }
+        }
+
+    }
+}
